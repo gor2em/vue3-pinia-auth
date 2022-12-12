7a1ba05b81a5af1bf206adca1dddb75309f7cd8a
gor2em <grkem.squash99@gmail.com>
2022-12-12 16:41:37 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Login from './views/Login.vue';
+
+</script>
+<template>
+
+  <router-view></router-view>
+
+</template>
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../store/auth";
+
+const routes = [
+  {
+    name: "login",
+    path: "/login",
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    name: "home",
+    path: "/home",
+    component: () => import("../views/Home.vue"),
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(),
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  const isAuth = authStore.isAuth;
+
+  const authNotRequired = ["login"];
+  const authRequired = ["home"];
+
+  if (authNotRequired.indexOf(to.name) > -1 && isAuth) {
+    next({ name: "home" });
+  }
+  console.log(authNotRequired.indexOf(to.name));
+  console.log(isAuth)
+
+  if (authRequired.indexOf(to.name) > -1) {
+    if (isAuth) {
+      next();
+    } else {
+      next({ name: "login" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
